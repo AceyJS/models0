64f470c75acbe42c30948acfb38dd9f900a310da
fantasim <siuol@gmx.com>
2022-04-25 08:40:39 +0300

    init

---
+++ loading-manager/index.ts
@@ -0,0 +1,29 @@
+import { Model } from 'acey'
+
+export type TLoading = 'default' | 'refresh' | false
+
+interface ILoadingManagerState {
+    loading: TLoading
+}
+
+const DEFAULT_STATE: ILoadingManagerState = {
+    loading: false
+}
+
+export default class LoadingManager extends Model {
+
+    static DefaultState: ILoadingManagerState = DEFAULT_STATE 
+
+    constructor(state: ILoadingManagerState = DEFAULT_STATE, options: any){
+        super(state, options)
+    }
+
+    public isLoading = () => this.state.loading != false
+    public isRefreshing = () => this.state.loading === 'refresh'
+    public isLoadingDefault = () => this.isLoading() && !this.isRefreshing()
+    public getLoadingStatus = () => this.state.loading as TLoading
+
+    public enable = (type: 'default' | 'refresh') => this.setState({loading: type}).save()
+    public disable = () => this.setState({loading: false}).save()
+}
+
