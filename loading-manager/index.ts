54242376901bda626811d77f7ae128397c65b977
fantasim <siuol@gmx.com>
2022-05-10 11:20:21 +0300

    has already loaded feature

--- loading-manager/index.ts
+++ loading-manager/index.ts
@@ -13,17 +13,21 @@ const DEFAULT_STATE: ILoadingManagerState = {
 export default class LoadingManager extends Model {
 
     static DefaultState: ILoadingManagerState = DEFAULT_STATE 
-
     constructor(state: ILoadingManagerState = DEFAULT_STATE, options: any){
         super(state, options)
+        this.setState({
+            has_loaded: state.loading
+        })
     }
+
+    public hasAlreadyLoaded = (): boolean => this.state.has_loaded
 
     public isLoading = () => this.state.loading != false
     public isRefreshing = () => this.state.loading === 'refresh'
     public isLoadingDefault = () => this.isLoading() && !this.isRefreshing()
     public getLoadingStatus = () => this.state.loading as TLoading
 
-    public enable = (type: 'default' | 'refresh') => this.setState({loading: type}).save()
+    public enable = (type: 'default' | 'refresh') => this.setState({loading: type, has_loaded: true}).save()
     public disable = () => this.setState({loading: false}).save()
 }
 
